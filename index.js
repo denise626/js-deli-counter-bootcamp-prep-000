856b2737272d3d195f276a0e21779cd3c6695127
April Kiser <denise626@hotmail.com>
2018-05-05 23:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
  }
 
-var line = [];
+var personInLine = [];
 
 
 function currentLine(katzDeli) {
@@ -29,5 +29,5 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else
-  return(`The line is currently:` + line);
+  return(`The line is currently:` + personInLine);
 }
