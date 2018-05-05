08ef14330cc4a3f231e09a441860dae48ba361fb
April Kiser <denise626@hotmail.com>
2018-05-05 23:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
-  return "Welcome, ${name}. You are number ${katzDeli.length} in line.`);"
+  return "Welcome, ${name}. You are number ${katzDeli.length} in line.`";
 }
+
