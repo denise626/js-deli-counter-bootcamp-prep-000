5a88b0b00ecede897ac38f96edac18b25504689d
April Kiser <denise626@hotmail.com>
2018-05-05 23:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function takeANumber(katzDeli, name) {
   return "Welcome, ${name}. You are number ${katzDeli.length} in line.`";
 }
 
+function nowServing(katzDeli) {
+  var i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
