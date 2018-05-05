cb746198a0afcdd671eacf35cade24196d53db65
April Kiser <denise626@hotmail.com>
2018-05-05 23:19:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  
+takeANumber(katzDeliLine, "Ada");
+takeANumber(katzDeliLine, "Grace");
+takeANumber(katzDeliLine, "Kent");
+
+currentLine(katzDeliLine);
+katzDeliLine.push(name);
+  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
+  
+}
+
+function nowServing() {
+  
+}
